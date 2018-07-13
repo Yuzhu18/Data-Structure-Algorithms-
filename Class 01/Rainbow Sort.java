7e471030ff2947dde72ebb3200a54d50bea766db
Yuzhu18 <40171739+Yuzhu18@users.noreply.github.com>
2018-07-13 15:55:16 -0700

    Create Rainbow Sort.java

---
+++ Class 01/Rainbow Sort.java
@@ -0,0 +1,34 @@
+public class Solution {
+
+  public int[] rainbowSort(int[] array) {
+  
+    if(array == null || array.length == 0){
+      return array;
+    }
+    
+    int neg = 0;
+    int pointer = 0;
+    int pos = array.length - 1;
+    
+    //三个挡板，四个区域，pointer扫未知区域
+    while(pointer <= pos){
+      if(array[pointer] == 0){
+        pointer++;
+      }else if(array[pointer] == -1){
+        swap(array, neg, pointer);
+        neg++;
+        pointer++;   //////和-1交换pointer也需要++
+      }else{
+        swap(array, pointer, pos);
+        pos--;
+      }
+    }
+    return array;
+  }
+  
+  private void swap(int[] array, int left, int right){
+    int temp = array[left];
+    array[left] = array[right];
+    array[right] = temp;
+  }
+}
