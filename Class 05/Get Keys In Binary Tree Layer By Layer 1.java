78d638e40c93b6955481f26ee6826e700493238c
Yuzhu18 <40171739+Yuzhu18@users.noreply.github.com>
2018-07-28 11:23:04 -0700

    Rename Get Keys In Binary Tree Layer By Layer.java to Get Keys In Binary Tree Layer By Layer 1.java

---
+++ Class 05/Get Keys In Binary Tree Layer By Layer 1.java
@@ -0,0 +1,42 @@
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+ 
+ 
+public class Solution {
+  public List<List<Integer>> layerByLayer(TreeNode root) {
+    // 注意要提前放好size
+    List<List<Integer>> rst = new ArrayList<>();
+    if(root == null){
+      return rst;
+    }
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    while(!queue.isEmpty()){
+      int size = queue.size();
+      List<Integer> curLayer = new ArrayList<>();
+      for(int i = 0; i < size; i++){
+        TreeNode cur = queue.poll();
+        curLayer.add(cur.key);
+        if(cur.left != null){
+          queue.offer(cur.left);
+        }
+        if(cur.right != null){
+          queue.offer(cur.right);
+        }
+      }
+     rst.add(curLayer);
+    }
+    return rst;
+  }
+}
+
+
+
