9533ef16c98b8b58f7c6ca8bb7a68b33c5922d2b
Yuzhu18 <40171739+Yuzhu18@users.noreply.github.com>
2018-08-02 14:37:19 -0700

    Create Missing Number I.java

---
+++ Class 07/Missing Number I.java
@@ -0,0 +1,37 @@
+public class Solution {
+  public int missing(int[] array) {
+    // 1. 求和相减
+    if(array.length == 0){
+      return 1;
+    }
+    int sum = 0;
+    int expect = (2 + array.length) * (array.length + 1) / 2;
+    for(int i = 0; i < array.length; i++){
+      sum = sum + array[i];
+    }
+    int rst = expect - sum;
+    return rst;
+  }
+}
+
+
+
+
+
+
+// 2. bit way
+
+public class Solution {
+    public int missing(int[] array) {
+        int xor = 0;
+        for (int i = 1; i <= array.length + 1; i++) {
+            xor ^= i;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            xor ^= array[i];
+        }
+
+        return xor;
+    }
+}
