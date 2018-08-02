05ab7ee8b80e2c7df9781e46dab0ec9f24da0f99
Yuzhu18 <40171739+Yuzhu18@users.noreply.github.com>
2018-08-02 14:38:04 -0700

    Create Common Numbers Of Two Sorted Arrays.java

---
+++ Class 07/Common Numbers Of Two Sorted Arrays.java
@@ -0,0 +1,22 @@
+public class Solution {
+  public List<Integer> common(List<Integer> A, List<Integer> B) {
+  
+    // two poniters
+    
+    List<Integer> rst = new ArrayList<>();
+    int pointA = 0;
+    int pointB = 0;
+    while(pointA < A.size() && pointB < B.size()){
+      if(A.get(pointA) == B.get(pointB)){
+        rst.add(A.get(pointA));
+        pointA++;
+        pointB++;
+      }else if(A.get(pointA) > B.get(pointB)){
+        pointB++;
+      }else{
+        pointA++;
+      }
+    }
+    return rst;
+  }
+}
