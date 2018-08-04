86e1c82d227ab128db2d8ff52c0d13c400064a7b
Yuzhu18 <40171739+Yuzhu18@users.noreply.github.com>
2018-08-04 11:38:38 -0700

    Create Determine If One String Is Another's Substring.java

---
+++ Class 07/Determine If One String Is Another's Substring.java
@@ -0,0 +1,31 @@
+public class Solution {
+  public int strstr(String large, String small) {
+    // 相当于两个For loop
+    if(small.length() > large.length()){
+      return -1;
+    }
+    if(small.length() == 0){
+      return 0;
+    }
+    char[] arrayL = large.toCharArray();
+    char[] arrayS = small.toCharArray();
+    for(int i = 0; i <= arrayL.length - arrayS.length; i++){ ////上限需要减small length 且 <=
+      if(equal(arrayL, i, arrayS)){
+         return i;
+      }
+    }
+    return -1;
+  }
+  private boolean equal(char[] large, int i, char[] small){
+    for(int j = 0; j < small.length; j++ ){
+      if(small[j] != large[i + j]){
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+
+
+
