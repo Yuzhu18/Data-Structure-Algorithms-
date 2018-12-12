dabe13e7374e0cb15dcf7c55c739c8afded94a53
Yuzhu18 <yuzhujia@usc.edu>
2018-12-11 22:57:36 -0800

    Update Merge Sort.java

--- Class 01/Merge Sort.java
+++ Class 01/Merge Sort.java
@@ -3,54 +3,43 @@ public class Solution {
  // 递归不是并行的，而是先运行到底
  // 每次二分排序（merge sort），谁小移谁合并（merge），最后复制回原来的array
  
-  public int[] mergeSort(int[] array) {
- 
-    if(array == null || array.length == 0){
-      return array;
-    }
-    int[] helper = new int[array.length];
-    mergesort(array, helper, 0, array.length-1);
+ public int[] mergeSort(int[] array) {
+    // Write your solution here
+    int[] rst = new int[array.length];
+    sort(array, rst, 0 , array.length - 1);
     return array;
   }
   
-  private void mergesort(int[] array, int[] helper, int left, int right){
+  private void sort(int[] array, int[] rst , int left, int right){
     if(left >= right){
       return;
     }
     int mid = left + (right - left) / 2;
-    mergesort(array, helper, left, mid);
-    mergesort(array, helper, mid+1, right);
-    merge(array, helper, left, right);
+    sort(array, rst, left, mid);
+    sort(array, rst, mid + 1, right);
+    merge(array, rst, left, right);
   }
   
-  private void merge(int[] array, int[] helper, int left, int right){
-    int leftIndex = left;
+  private void merge(int[] array,int[] rst, int left, int right){
     int mid = left + (right - left) / 2;
-    int rightIndex = mid + 1;
-    int i = left;
-    while(leftIndex <= mid && rightIndex <= right){
-      if(array[leftIndex] < array[rightIndex]){
-        helper[i] = array[leftIndex];
-        leftIndex++;
+    int l = left;
+    int r = mid + 1;
+    int index = left;        /////////index 起始是left
+    while(l <= mid && r <= right){
+      if(array[l] <= array[r]){
+        rst[index++] = array[l++];
       }else{
-        helper[i] = array[rightIndex];
-        rightIndex++;
+        rst[index++] = array[r++];
       }
-      i++;
     }
-    while(leftIndex <= mid){
-      helper[i] = array[leftIndex];
-      leftIndex++;
-      i++;
+    while(l <= mid){
+      rst[index++] = array[l++]; ////////// array[l ++] 记得++
     }
-    while(rightIndex <= right){
-      helper[i] = array[rightIndex];
-      rightIndex++;
-      i++;
+    while(r <= right){
+      rst[index++] = array[r++];  ////////// array[r ++] 记得++
     }
     
-    for(int j = left; j <= right ; j++){
-      array[j] = helper[j];
-    }
+    for( int i = left; i <= right ; i++){  ////// 注意是left - right
+            array[i] = rst[i];
+     }
   }
-}
