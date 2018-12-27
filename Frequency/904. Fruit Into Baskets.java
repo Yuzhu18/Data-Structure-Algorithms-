7d687ab24948cfb831bf2342b94b9496aa37f485
Yuzhu18 <yuzhujia@usc.edu>
2018-12-27 12:47:31 -0800

    Create 904. Fruit Into Baskets.java

---
+++ Frequency/904. Fruit Into Baskets.java
@@ -0,0 +1,29 @@
+//similar to longest unrepeated characters
+//time: loop once O(n)
+//space: map O(n)
+
+class Solution {
+    public int totalFruit(int[] tree) {
+        if (tree == null || tree.length == 0) {
+            return 0;
+        }
+        
+        Map<Integer, Integer> hashmap = new HashMap<>();
+        int start = 0;
+        int max = 0;
+        
+        for (int end = 0; end < tree.length; end++) {
+            hashmap.put(tree[end], hashmap.getOrDefault(tree[end], 0) + 1);
+            
+            while (hashmap.size() > 2) {
+                hashmap.put(tree[start], hashmap.get(tree[start]) - 1);                
+                if (hashmap.get(tree[start]) == 0) {
+                    hashmap.remove(tree[start]);
+                }
+                start++;
+            }
+            max = Math.max(end - start + 1, max);            
+        }
+        return max;
+    }
+}
