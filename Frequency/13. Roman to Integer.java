69a85457497d742277736c16357d0446419fcf7f
Yuzhu18 <yuzhujia@usc.edu>
2018-12-28 23:38:37 -0800

    Create 13. Roman to Integer.java

---
+++ Frequency/13. Roman to Integer.java
@@ -0,0 +1,30 @@
+class Solution {
+    // time: one pass O(n)
+    // space: map  O(1)
+    public int romanToInt(String s) {
+        int rst = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+            
+        int last = map.get(s.charAt(0));
+        rst += last;
+        for(int i = 1; i < s.length(); i++){
+            int cur = map.get(s.charAt(i));
+            
+            if(last >= cur){
+                rst += cur;
+            }else{
+                rst = rst  + cur - last -last; 
+            }
+            last = cur;
+        }
+        return rst;
+    }
+}
