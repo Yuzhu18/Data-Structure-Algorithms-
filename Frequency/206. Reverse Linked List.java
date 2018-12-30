72b2a3cdfa5e49242d279925af4ec649bb21f8db
Yuzhu18 <yuzhujia@usc.edu>
2018-12-29 17:04:45 -0800

    Create 206. Reverse Linked List.java

---
+++ Frequency/206. Reverse Linked List.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        // iterative way
+        //corner case
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode cur = head;
+        ListNode prev = null;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        
+        return prev;
+    }
+}
