da8bb3b50f07d7c1350a816fc7d90c8073d755ed
Yuzhu18 <yuzhujia@usc.edu>
2019-01-10 23:59:42 -0800

    Create RH_TwoSum.java

---
+++ Frequency/RH_TwoSum.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    private static List<Integer> list = new ArrayList<Integer>();
+
+    private static void store(int input){
+        list.add(input);
+    }
+
+    private static int twoSum(int target){
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < list.size(); i++ ){
+            int n = target - list.get(i);
+            if(!set.contains(n)){
+                set.add(n);
+            }else{
+                return i;
+            }
+        }
+        
+    }
+}
