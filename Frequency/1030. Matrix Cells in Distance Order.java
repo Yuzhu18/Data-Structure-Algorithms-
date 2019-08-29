eec2c3ccd8c6f156d3c2ea8930c4d25f87315820
Yuzhu18 <yuzhujia@usc.edu>
2019-08-28 22:22:46 -0700

    Create 1030. Matrix Cells in Distance Order.java

---
+++ Frequency/1030. Matrix Cells in Distance Order.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{r0, c0});
+        boolean[][] visited = new boolean[R][C];
+        int[][] result = new int[R * C][2];
+        int index = 0;
+        while(!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int curR = cell[0];
+            int curC = cell[1];
+            if (curR < 0 || curR >= R || curC < 0 || curC >= C) {
+                continue;
+            }
+            if (visited[curR][curC] == true) {
+                continue;
+            }
+            
+            visited[curR][curC] = true;
+            result[index] = cell;
+            index++;
+            queue.offer(new int[]{curR - 1, curC});
+            queue.offer(new int[]{curR + 1, curC});
+            queue.offer(new int[]{curR, curC - 1});
+            queue.offer(new int[]{curR, curC + 1});
+        }
+        return result;
+    }
+}
