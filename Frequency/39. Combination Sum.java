cf2b8eae91cf08423767a9b22467fe3b0d5fb2fb
Yuzhu18 <yuzhujia@usc.edu>
2019-10-18 16:47:38 -0700

    Update 39. Combination Sum.java

--- Frequency/39. Combination Sum.java
+++ Frequency/39. Combination Sum.java
@@ -1,7 +1,9 @@
+
 class Solution {
     // DFS backtracking 
     // time: 2^n
     // space: recursion O(n)
+    // pruning 减支来提升time complexcity
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> rst = new ArrayList<>();
         Arrays.sort(candidates);
@@ -9,32 +11,20 @@ class Solution {
         return rst;
     }
     
-    private void helper(List<List<Integer>> rst, int[] candidates, List<Integer> temp, int remain, int start){
-        if(remain < 0){return; }
-        else if(remain == 0){
-            // temp每次都在变
-            rst.add( new ArrayList<Integer>(temp));
-        }else{
-            for(int i = start; i < candidates.length; i++){
-                temp.add(candidates[i]);
-                
-                helper(rst, candidates, temp, remain - candidates[i], i);// not i + 1 because it can be repeatedly used
-                temp.remove(temp.size() - 1);
+    private void helper(List<List<Integer>> rst, int[] candidates, List<Integer> cur, int target, int start){
+        if(target == 0){
+            rst.add(new ArrayList<Integer>(cur));
+            return;
+        }
+        for(int i = start; i < candidates.length; i++){
+            // 减支 ： 如果当前candidate 大于target，那么就不会有符合条件的组合了
+            if (candidates[i] > target) {
+                break;
             }
+            cur.add(candidates[i]);
+            // not i + 1 because it can be repeatedly used
+            helper(rst, candidates, cur, target - candidates[i], i);
+            cur.remove(cur.size() - 1);
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
