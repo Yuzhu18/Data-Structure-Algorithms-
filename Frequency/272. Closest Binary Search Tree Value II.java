44ad0692a2fbe3f3e61bdebb078ff3d8e5afd919
Yuzhu18 <yuzhujia@usc.edu>
2019-11-17 15:13:47 -0800

    Create 272. Closest Binary Search Tree Value II.java

---
+++ Frequency/272. Closest Binary Search Tree Value II.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // 思路是in-order 遍历 tree 维护一个 k-size queue, 
+    // 每次比较queue的头和target的接近程度, 最后返回这个queue里的k个元素
+    // Time: O(n) -- total tree node
+    // Space: O(tree height)
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        Deque<Integer> queue = new LinkedList<>();
+        traverse(root, target, k, queue);
+        // store rst
+        List<Integer> rst = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            rst.add(queue.pollFirst());
+        }
+        return rst;
+    }
+    
+    private void traverse(TreeNode root, double target, int k, Deque<Integer> queue) {
+        // stop case:
+        if (root == null) {
+            return;
+        }
+        traverse(root.left, target, k, queue);
+        if (queue.size() < k) {
+            queue.offerLast(root.val);
+        } else {
+            int smallest = queue.getFirst();
+            if (Math.abs(root.val - target) < Math.abs(smallest - target)) {
+                queue.pollFirst();
+                queue.offerLast(root.val);
+            }
+        }
+        traverse(root.right, target, k, queue);
+    }
+}
